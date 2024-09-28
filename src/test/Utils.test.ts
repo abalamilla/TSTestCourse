c6194bd291dfe5ba48de1ccbf0e9ce45b2c91f3a
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-09-28 11:37:40 -0600

    🧪 different assertions and matchers
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of valid string", () => {
@@ -11,5 +11,26 @@ describe("Utils test suite", () => {
     //
     // assert
     expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("Hello");
+
+    expect(actual.lowerCase).toBe("hello");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(5);
+    expect(actual.characters).toHaveLength(5);
+
+    expect(actual.characters).toEqual(["H", "e", "l", "l", "o"]);
+    expect(actual.characters).toContain<string>("H");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["e", "H", "o", "l"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
